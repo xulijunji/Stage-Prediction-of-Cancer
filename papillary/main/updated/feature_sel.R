48b8c6269821bd053f005034bda6347d843523a7
NPSDC <noorpratap.singh@research.iiit.ac.in>
2017-02-28 12:03:52 +0530

    feature sel for varselRF comp and deseq2 initiated

--- papillary/main/updated/feature_sel.R
+++ papillary/main/updated/feature_sel.R
@@ -4,21 +4,22 @@ source('main/final.R')
 #library(pamr)
 
 load('environment/stages.level.comb.RData')
+load('environment/accuracy_feature/dds_nor_tum_comb.RData')
 load('environment/accuracy_feature/vs_nor_comb.RData')
 load('environment/accuracy_feature/tumor_ind_vs.RData')
 load('environment/first_trial_shrunken_classifier.RData')
 load('environment/accuracy_feature/updated/gr.RData')
 load('environment/accuracy_feature/updated/net_features_updated.RData')
+load('environment/accuracy_feature/net_features.RData')
 
 vst_tumor_tum <- vs_normal_comb_reported[tumor.ind.vs, ]
 
 gr.updated <- build.groups(260,5)
 stage.dist <- get.stage.distribution(gr.updated, stages.levels.comb)
+gr.updated.train <- gr.updated[-1]
 
 test.group <- gr.updated[[1]]
 train.group <- unlist(gr.updated[-1])
-
-gr.train <- gr.updated[-1] ##in terms of 260
 
 net.features.updated <- list()
 ######Shrunken####
@@ -31,7 +32,32 @@ net.features.updated$shrunken[['atleast_2']] <- get.genes.common(net.features.up
 net.features.updated$shrunken[['atleast_4']] <- get.genes.common(net.features.updated$shrunken$genes, 4)
 net.features.updated$shrunken[['genes_stage']] <- get.shrunken.group.stage(net.features.updated$shrunken$genes.object)
 net.features.updated$shrunken[['atleast_dfs']] <- get.shrunken.common.stage(net.features.updated$shrunken$genes_stage, c(1,2,4))
+save(net.features.updated,file = 'environment/accuracy_feature/updated/net_features_updated.RData')
 
 ######VarSelRF####
 net.features.updated[['varSelRF']] <- list()
 net.features.updated[['varSelRF']][['genes.object']] <- do.varselRF(gr.train, vst_tumor_tum, stages.levels.comb)
+net.features.updated$varSelRF[['genes.list']] <- get.min.oob.varselRf(net.features.updated$varSelRF$genes.object)
+net.features.updated$varSelRF[['atleast_1']] <- get.genes.common(net.features.updated$varSelRF$genes.list,1)
+net.features.updated$varSelRF[['atleast_2']] <- get.genes.common(net.features.updated$varSelRF$genes.list,2)
+net.features.updated$varSelRF[['atleast_3']] <- get.genes.common(net.features.updated$varSelRF$genes.list,3)
+net.features.updated$varSelRF[['atleast_4']] <- get.genes.common(net.features.updated$varSelRF$genes.list,4)
+
+######DESeq2
+load('environment/accuracy_feature/updated/dds_tumor_reported.RData')
+net.features.updated[['deseq2']] <- list()
+sample.df <- colData(dds_tumor_reported)
+sample.df[,3] <- sapply(as.character(sample.df[,3]), function(stage)
+{
+  if(stage == 'stage ii')
+    stage = 'stage i'
+  else if(stage == 'stage iii')
+    stage = 'stage iv'
+  else
+    stage = stage
+})
+sample.df$stage <- as.factor(sample.df$stage)
+
+dds_obj <- create.Deseq2(gr.updated.train, counts(dds_tumor_reported), 
+                         colData = sample.df)
+res.train.dds_obj <- do.Deseq2(dds_obj)
